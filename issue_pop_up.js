6e4bfe7545238873c8b77ab936c13263e4e8aeea
WutangchickeN <34319000+WutangchickeN@users.noreply.github.com>
2019-11-15 18:19:11 -0800

    Altered about page

--- issue_pop_up.js
+++ issue_pop_up.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 function popUp()
 {
     alert("Placeholder for Pop Up Information once the user has selected an entry from the Database Table.");
 
 return false;
 }
+=======
+function popUp()
+{
+    alert("Placeholder for Pop Up Information once the user has selected an entry from the Database Table.");
+
+return false;
+}
+>>>>>>> 6159c52890de1232462386142f0d4e144f1cc1fd
